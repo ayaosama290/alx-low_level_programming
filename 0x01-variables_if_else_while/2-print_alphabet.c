82189109b63c59d80687e3d69b6bd5adb7ad01cf
ayaosama290 <orakel999@gmail.com>
2023-08-13 06:12:54 -0700

    g

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a'
+	char end_alpha = 'z'
+	while (ch <= end_alpha)
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
