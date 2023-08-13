4e7e6c14f2df563ca481b63f54a600ba004e4484
ayaosama290 <orakel999@gmail.com>
2023-08-13 07:56:00 -0700

    h

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		int putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
