26dde76475ec46db5268be07623237b8520eab2b
ayaosama290 <orakel999@gmail.com>
2023-08-12 17:56:46 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-	char first[] = "\"Programming is like building a multilingual puzzle\n";
+	char first[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(first);
 	return (0);
