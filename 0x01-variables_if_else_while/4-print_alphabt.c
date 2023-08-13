127f3dd485d53b23779993f3b8520dead40b5bf9
ayaosama290 <orakel999@gmail.com>
2023-08-13 06:53:29 -0700

    h

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+	char end_ch = 'z';
+
+	while (ch <= end_ch)
+	{
+		if (ch != q || ch != e)
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
