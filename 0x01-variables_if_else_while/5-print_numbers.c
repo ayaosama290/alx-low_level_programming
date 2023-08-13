21bc33f16fd3b68679ddcb040cadd2654c5410de
ayaosama290 <orakel999@gmail.com>
2023-08-13 07:38:11 -0700

    h

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	short int num = 0;
+	while (num < 10)
+	{
+		printf(num);
+		num++;
+	}
+	printf('\n');
+	return (0);
+}
