b4f404be37a153915faf8671b5994fc56d59f1b0
ayaosama290 <orakel999@gmail.com>
2023-08-13 08:34:18 -0700

    k

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main()
+{
+	int num = 0;
+	char ch = 'a';
+	char ch_end = 'f';
+	while (num < 10)
+	{
+		putchar('0' + num);
+		num++;
+	}
+	while (ch < ch_end)
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
