c8c8edfcfba07fe270a7233abb61abeb768272e1
ayaosama290 <orakel999@gmail.com>
2023-08-12 19:55:19 -0700

    f

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	static const char example[] = "and that pieceof art is useful\" - Dora Korpar,2015-10-19\n";
+	fwrite(example)
+	return (1);
+}
