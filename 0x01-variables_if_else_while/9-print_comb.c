fdbe59b9362b9c696160d6a3b7834287e24b45fd
ayaosama290 <orakel999@gmail.com>
2023-08-13 09:11:45 -0700

    h

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10;num++)
+	{
+		putchar('0' + num);
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
