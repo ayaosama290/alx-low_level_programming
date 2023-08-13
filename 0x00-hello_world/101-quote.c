0c96d7589152233292e7554173573fb1054bf642
ayaosama290 <orakel999@gmail.com>
2023-08-12 20:06:14 -0700

    g

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-	static const char example[] = "and that piece of art is " + 
-	       	"useful\" - Dora Korpar, 2015-10-19\n";
+	static const char example[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fwrite(example, sizeof(example) - 1, 1, stdout);
 	return (1);
