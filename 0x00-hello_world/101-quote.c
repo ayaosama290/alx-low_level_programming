e91ed5d25468a5c266d4e99130dc4469ce3900de
ayaosama290 <orakel999@gmail.com>
2023-08-12 19:57:43 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 	static const char example[] = "and that pieceof art is useful\" - Dora Korpar,2015-10-19\n";
-	fwrite(example)
+	fwrite(example, sizeof(example) -1, 1, stdout);
 	return (1);
 }
