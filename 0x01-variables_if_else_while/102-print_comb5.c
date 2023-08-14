78aa576a57dcb2151a5809a4c4ee1f7d8ec024b9
ayaosama290 <orakel999@gmail.com>
2023-08-14 16:09:18 -0700

    g

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: 0
-*/
+ */
 int main(void)
 {
-    int a, b;
+	int a;
+	int b;
 
-    for (a = 0; a <= 99; a++)
-        {
-            for (b = a + 1; b <= 99; b++)
-            {
-                putchar((a / 10) + '0');
-                putchar((a % 10) + '0');
-                putchar(' ');
-                putchar((b / 10) + '0');
-                putchar((b % 10) + '0');
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
 
-                if (a < 98 || b < 99)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-            putchar('\n');
-            return (0);
+			if (a < 98 || b < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
