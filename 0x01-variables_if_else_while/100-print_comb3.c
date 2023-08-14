55ad7935b1536ab3d6315b75a126fc1fd5599ce5
ayaosama290 <orakel999@gmail.com>
2023-08-14 13:54:16 -0700

    g

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,6 +8,19 @@ int main(void)
 {
 	int first_digit;
 	int second_digit;
-	for (){}
+	for (first_digit = 0; first_digit <= 9; first_digit++)
+	{
+		for (second_digit = 0; second_digit <= 9; second_digit++)
+		{
+			putchar(first_digit + '0');
+			putchar(second_digit + '0');
+			if (first_digit < 8 || second_digit < 9 )
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
