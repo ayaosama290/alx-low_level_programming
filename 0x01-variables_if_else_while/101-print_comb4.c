70e9ab97e87d6a5c946f7f290352bad2550c2f7f
ayaosama290 <orakel999@gmail.com>
2023-08-14 14:21:12 -0700

    h

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,7 +18,7 @@ int main(void)
 				putchar(a + '0');
 				putchar(b + '0');
 				putchar(c + '0');
-				if (a < 9 || b < 8 || c < 7)
+				if (a < 7 || b < 8 || c < 9)
 				{
 					putchar(',');
 					putchar(' ');
