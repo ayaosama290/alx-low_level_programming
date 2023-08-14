f961f8cf6554fb238dc34543e9844b2493a767dc
ayaosama290 <orakel999@gmail.com>
2023-08-14 13:59:57 -0700

    j

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 	int second_digit;
 	for (first_digit = 0; first_digit <= 9; first_digit++)
 	{
-		for (second_digit = 0; second_digit <= 9; second_digit++)
+		for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
 		{
 			putchar(first_digit + '0');
 			putchar(second_digit + '0');
