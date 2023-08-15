80475344e28dc94d79986cd8a6575a7b1d999041
ayaosama290 <orakel999@gmail.com>
2023-08-15 10:32:18 -0700

    k

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	_putchar("_putchar");
-	_putchar('\n');
+	putchar("_putchar");
+	putchar('\n');
 	return (0);
 }
