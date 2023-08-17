49550cf52d0ebb7850685a1de9093d9ec47f6930
ayaosama290 <orakel999@gmail.com>
2023-08-17 14:49:54 -0700

    j

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,72 +9,72 @@
  */
 void print_to_98(int n)
 {
-	while(n != 98)
-    {
-        for (;n < -9 && n >= -99; n++)
-        {
-            int neg = -1 * n;
-            putchar('-');
-            putchar((neg / 10) + 48);
-            putchar((neg % 10) + 48);
-            neg = -1 * n;
-            putchar(',');
-            putchar(' ');
-        }
-        for (; n < -99; n++)
-        {
+	while (n != 98)
+	{
+        	for (;n < -9 && n >= -99; n++)
+        	{
+            	   int neg = -1 * n;
+           	   putchar('-');
+            	   putchar((neg / 10) + 48);
+            	   putchar((neg % 10) + 48);
+            	   neg = -1 * n;
+            	   putchar(',');
+            	   putchar(' ');
+        	}
+        	for (; n < -99; n++)
+        	{
+	
+        	    int neg = -1 * n;
+           	    putchar('-');
+            	    putchar((neg / 100) + 48);
+            	    putchar(((neg / 10) % 10) + 48);
+            	    putchar((neg % 10) + 48);
+                    neg = -1 * n;
+                    putchar(',');
+                    putchar(' ');
 
-            int neg = -1 * n;
-            putchar('-');
-            putchar((neg / 100) + 48);
-            putchar(((neg / 10) % 10) + 48);
-            putchar((neg % 10) + 48);
-            neg = -1 * n;
-            putchar(',');
-            putchar(' ');
+        	}
+        	for (;n < 0 && n >= -9; n++)
+        	{
+            	   int neg = -1 * n;
+            	   putchar('-');
+           	   putchar((neg % 10) +48);
+            	   neg = -1 * n;
+           	   putchar(',');
+            	   putchar(' ');
+       		 }
+        	for (; n > 98; n--)
+        	{
+               	   if (n > 99)
+                   {
+                	putchar((n / 100) +48);
+                	putchar(((n / 10) % 10) + 48);
+               		putchar((n % 10) + 48);
+                	putchar(',');
+               	        putchar(' ');
+            	   }
+                   else
+               	   {
+                       putchar((n / 10) + 48);
+                       putchar((n % 10) + 48);
+                       putchar(',');
+                       putchar(' ');
+            	   }
 
-        }
-        for (;n < 0 && n >= -9; n++)
-        {
-            int neg = -1 * n;
-            putchar('-');
-            putchar((neg % 10) +48);
-            neg = -1 * n;
-            putchar(',');
-            putchar(' ');
-        }
-        for (; n > 98; n--)
-        {
-            if (n > 99)
-            {
-                putchar((n / 100) +48);
-                putchar(((n / 10) % 10) + 48);
-                putchar((n % 10) + 48);
-                putchar(',');
-                putchar(' ');
-            }
-            else
-            {
-                putchar((n / 10) + 48);
-                putchar((n % 10) + 48);
-                putchar(',');
-                putchar(' ');
-            }
-
-        }
-        for (; n >= 10 && n < 98; n++)
-        {
-            putchar((n / 10) + 48);
-            putchar((n % 10) + 48);
-            putchar(',');
-            putchar(' ');
-        }
-        for (; n >= 0 && n <= 9; n++)
-        {
-            putchar((n % 10) + 48);
-            putchar(',');
-            putchar(' ');
-        }
+        	}
+        	for (; n >= 10 && n < 98; n++)
+        	{
+            	    putchar((n / 10) + 48);
+                    putchar((n % 10) + 48);
+                    putchar(',');
+                    putchar(' ');
+                }
+       	        for (; n >= 0 && n <= 9; n++)
+        	{
+            	    putchar((n % 10) + 48);
+                    putchar(',');
+                    putchar(' ');
+        	}
     }
     putchar(9 + '0');
     putchar(8 + '0');
