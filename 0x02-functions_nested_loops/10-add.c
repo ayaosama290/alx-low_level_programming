af7965218e57a6ea5089bd991b6e902b770bf2d9
ayaosama290 <orakel999@gmail.com>
2023-08-17 11:28:43 -0700

    k

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ *
+ * Description: a function that adds two numbers abd return result
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: 0 if success
+ */
+int add(int a, int b)
+{
+	res = a + b;
+	_putchar(res + 48);
+	return (0);
+}
