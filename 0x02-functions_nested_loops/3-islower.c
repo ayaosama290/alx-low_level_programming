f94a777d3d0558c700552d1adae1b30ca2c62d8f
ayaosama290 <orakel999@gmail.com>
2023-08-16 22:35:15 -0700

    L

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _islower - function that checks letters
+ *
  * @c: parameter that has the checked letter 
  * Description: is a letter capital or not
- * Return :0
  *
+ * Return :0
  */
-
 int _islower(int c)
 {
 	if (c >= 'A' && c <= 'Z')
