303f7254a087de889ed61184cea42dddba81eb7c
ayaosama290 <orakel999@gmail.com>
2023-08-17 13:21:36 -0700

    h

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,8 +14,11 @@ void print_to_98(int n)
 		{
 			_putchar((n / 10) + 48);
 			_putchar((n % 10) + 48);
-			_putchar(',');
-			_putchar(' ');
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
