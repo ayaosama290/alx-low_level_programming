463a2e37458cd50810647d10e117ae9532cb19bb
ayaosama290 <orakel999@gmail.com>
2023-08-18 01:28:16 -0700

    k

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,40 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+    int largest;
 
-	if (a > b)
-	{
-		if (a > c)
-		{
-			largest = a;
-		}
-		else
-		{
-			largest = c;
-		}
-	}
-	else if (b > a)
-	{
-		if (b > c)
-		{
-			largest = b;
-		}
-		else
-		{
-			largest = c;
-		}
-	}
-	else if (c > a)
-	{
-		if (c > b)
-		{
-			largest = c;
-		}
-		else
-		{
-			largest = b;
-		}
-	}
-	return (largest);
+    if (a >= b && a >= c)
+    {
+        largest = a;
+    }
+    else if (b >= a && b >= c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
 }
