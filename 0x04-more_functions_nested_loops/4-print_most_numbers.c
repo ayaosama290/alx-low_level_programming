f2fecc1baeb75e6e7e30289079d38ec1946872a6
ayaosama290 <orakel999@gmail.com>
2023-08-19 01:07:17 -0700

    k

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints most numbers
+ *
+ * Description: a function that prints numbers 0-9 excluding 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int n;
+	for (n = 0; n <= 9; n++ )
+	{
+		if (n == 2 || n == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(n + 48);
+		}
+	}
+	_putchar('\n');
+
+}
