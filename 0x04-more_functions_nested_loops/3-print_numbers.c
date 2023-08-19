7341631759b48f5f18b096a6f857b8ad983a3484
ayaosama290 <orakel999@gmail.com>
2023-08-19 00:49:39 -0700

    g

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ *
+ * Description: a function that prints numbers 0 - 9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
