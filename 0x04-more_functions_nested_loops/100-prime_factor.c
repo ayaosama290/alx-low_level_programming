23fac44dc7abf2f85ed25979419d18b73103ebd2
ayaosama290 <orakel999@gmail.com>
2023-08-19 12:24:14 -0700

    l

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -14,20 +14,20 @@ int main(void)
 
 	while (n % 2 == 0)
 	{
-		printf("%d ", 2);
+		printf("%d,", 2);
 		n = n / 2;
 	}
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
-			printf("%d ", i);
+			printf("%d,", i);
 			n = n / i;
 		}
 	}
 	if (n > 2)
 	{
-		printf("%ld ", n);
+		printf("%ld,", n);
 	}
 	printf("\n");
 	return (0);
