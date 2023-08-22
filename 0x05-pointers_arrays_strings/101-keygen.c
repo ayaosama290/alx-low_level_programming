f3d258ca25bab9b20c362810524f2fb243a292ef
ayaosama290 <orakel999@gmail.com>
2023-08-21 20:14:52 -0700

    h

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int s;
+	char c;
+
+	srand(time(NULL));
+	while (s <= 2645)
+	{
+		c = rand() % 128;
+		s += c;
+		putchar(c);
+	}
+	putchar(2772 - s);
+	return (0);
+}
