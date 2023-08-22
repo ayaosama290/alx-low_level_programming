1b74520cac0e0c8e4f461a74ad116c61727611e4
ayaosama290 <orakel999@gmail.com>
2023-08-21 18:40:45 -0700

    h

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps numbers
+ *
+ * Description: a function that swaps numbers a and b
+ * @a: the first parameter
+ * @b: the second parameter
+ */
+void swap_int(int *a,int *b)
+{
+	*a = 42;
+	*b = 98;
+}
