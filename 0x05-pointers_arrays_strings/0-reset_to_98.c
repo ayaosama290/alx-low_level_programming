fbab11000dd7a3e73b74f9dbfc6c499ba09cffae
ayaosama290 <orakel999@gmail.com>
2023-08-21 18:17:44 -0700

    l

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * resert_to_98 - reset pointer
  *
- * Description: 'a function that resets a pointer to the value 98 blah blah blah hhhh '
+ * Description: 'a function that resets a pointer to the value 98'
  * @n: the first parameter
  */
 void reset_to_98(int *n)
