7783199024e1931e44b167670be95d61c90c0e8d
ayaosama290 <orakel999@gmail.com>
2023-08-21 18:28:38 -0700

    f

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * resert_to_98 - reset pointer to number 98 after calling
  *
- * @n: the first parameter
+ * @*n: the first parameter
  * Return: nothing
  *
  * Description: dfjldafh dklhfk lhfklh dh fk h fkl shf hfk hklgahf
