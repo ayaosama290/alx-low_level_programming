4e4c63a3a038847713a9cbf20227a05516d381e1
ayaosama290 <orakel999@gmail.com>
2023-08-21 19:12:19 -0700

    k

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - string length
+ *
+ * Description: a function that counts the string length
+ * @s: the first parameter
+ *
+ * Return: 0 succeed
+ */
+int _strlen(char *s)
+{
+	 int i;
+
+        for (i = 0; *s != '\0' ; s++)
+        { 
+            ++i;
+        }
+        return (i);
+}
