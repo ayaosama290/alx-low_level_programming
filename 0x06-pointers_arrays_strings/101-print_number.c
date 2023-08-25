60c90fc924a9ed400c6d161d74da8740b0b1aec6
ayaosama290 <orakel999@gmail.com>
2023-08-25 00:28:12 -0700

    l

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number - prints integer
+ *
+ * Description: a function that prints an integer dfjklsdk
+ * @n: the first parameter
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
