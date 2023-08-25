2b416c2b405531f862db71c2225f39ce2efb3b6f
ayaosama290 <orakel999@gmail.com>
2023-08-24 17:53:30 -0700

    j

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * isLower - determine if lower
+ * @c: parameter
+ *
+ * Return: 1 or 0
+ */
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+/**
+ * isDelimiter - determine delimit
+ * @c: parameter
+ * Return: 1 or 0
+ */
+int isDelimiter(char c)
+{
+	int ii;
+	char delimiter[] = " \t\n,.!?\"()[}";
+
+	for (ii = 0; ii < 12; ii++)
+	{
+		if (c == delimiter[ii])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+/**
+ * cap_string - capitalize words
+ *
+ * Description: a function that capitalize words dkfjkdfh fjdk hfks
+ * @s: parameter
+ *
+ * Return: a character
+ */
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int foundDelimit = 1;
+
+	while (*s)
+	{
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
+		{
+			*s -= 32;
+			foundDelimit 0;
+		}
+		else
+			foundDelimit = 0;
+		s++;
+	}
+	return (ptr);
+}
