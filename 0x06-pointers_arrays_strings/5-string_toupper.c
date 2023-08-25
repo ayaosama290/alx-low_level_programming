9ee277620c723dd4a13b41a03c8b1476d604e057
ayaosama290 <orakel999@gmail.com>
2023-08-24 17:33:52 -0700

    l

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - to uppercase
+ *
+ * Description: a function that changes from lower to upper
+ *@: the only parameter
+ *
+ * Return: a character
+ */
+char *string_toupper(char *)
+{
+	int ii;
+
+	for (ii = 0; str[ii] != '\0'; ii++)
+	{
+		if (str[ii] >= 97 && str[ii] <= 122)
+		{
+			str[ii] = str[ii] - 32;
+		}
+	}
+	return (str);
+}
