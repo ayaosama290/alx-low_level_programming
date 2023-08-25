871a8d85c42bcbef2f24c2eb84910ad1f0a058db
ayaosama290 <orakel999@gmail.com>
2023-08-24 17:05:59 -0700

    j

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - concatenates
+ *
+ * Description: a function that concatinates strings together
+ * @dest: the first parameter
+ * @src: the second parameter
+ * @n: the third parameter
+ *
+ * Return: a character
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int cat, i;
+	cat = 0;
+	while (dest[cat])
+	{
+		cat++
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[cat + i] = src[i];
+	}
+	dest[cat + i] = '\0';
+	return (dest);
+}
