0e3a6e897fafce453ad9a56e3ad6bfa11a6e4cf6
ayaosama290 <orakel999@gmail.com>
2023-08-28 09:46:11 -0700

    k

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
- * _memset: file memory
+ * _memset - file memory
+ *
+ * Description: a function that does something with file memory
  * @s: the first parameter
  * @b: the second parameter
  * @n: the third parameter
+ * Return: a character
  */
-char *_memset(char *s,char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int itr;
 
