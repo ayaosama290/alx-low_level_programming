f623dd8b21bb144d2cd2d31ab2e6bb1b9e1da2d5
ayaosama290 <orakel999@gmail.com>
2023-10-03 23:44:30 -0700

    j

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: a function that returns the number of entered arguments
+ * @argc: the first parameter
+ * @argv: the second parameter
+ * Return: 0 on SUCCESS
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return 0;
+}
