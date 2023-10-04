230b2381a5a15e026e2d3250ac5eef36e8e13235
ayaosama290 <orakel999@gmail.com>
2023-10-03 23:38:04 -0700

    h

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - Entry point
+ * Description: the main function using argc and argv
+ * @argc: the first parameter
+ * @argv: the second parameter
+ * Return: 0 on SUCCESS
+ */
 int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[argc -1]);
+	printf("%s\n", argv[argc - 1]);
 
-    return 0;
+	return (0);
 }
