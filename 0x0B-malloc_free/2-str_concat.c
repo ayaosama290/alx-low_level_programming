cd7eb2dd0a0bb3d8172ddae7224978d38b44a10c
ayaosama290 <orakel999@gmail.com>
2023-10-14 01:01:58 -0700

    h

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,34 +11,31 @@
 char *str_concat(char *s1, char *s2)
 {
 	int size1 = 0, size2 = 0;
-	int i = 0, j = 0;
+	int i = 0;
 	char *m;
 
+	if (s1 == NULL)
+	{
+		s1 = "\0";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "\0";
+	}
 	for (; s1[size1] != '\0'; size1++)
 	{}
 	for (; s2[size2] != '\0'; size2++)
 	{}
-	if (s1 == NULL)
-	{
-		s1 = '\0';
-	}
-	if (s2 == NULL)
-	{
-		s2 = '\0';
-	}
 	m = malloc((size1 + size2) * sizeof(char) + 1);
 	if (m == 0)
-	{
 		return (0);
-	}
-
-	for (; i < size1; i++)
+	for (; i <= size1 + size2; i++)
 	{
-		m[i] = s1[i];
+		if (i < size1)
+			m[i] = s1[i];
+		else
+			m[i] = s2[i - size1];
 	}
-	for (; j < size2; j++)
-	{
-		m[i + j] = s2[j];
-	}
+	m[i] = '\0';
 	return (m);
 }
