0e9a92a4fb1ab085b88cab842d411f57c9fcb96f
ayaosama290 <orakel999@gmail.com>
2023-10-17 09:14:50 -0700

    d

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - calloc function
+ * @nmemb: the first parameter
+ * @size: the second parameter
+ * Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *m = calloc(nmemb, size);
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	return (m);
+}
