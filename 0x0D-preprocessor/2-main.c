ce97713b73e2f58003d0537364bf91e5749989e7
ayaosama290 <orakel999@gmail.com>
2023-10-23 13:05:07 -0700

    h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
-#ifndef MAIN_C
-#define MAIN_C
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * main -Entry point
+ * Description: the main function
+ * Return: void
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
-#endif
