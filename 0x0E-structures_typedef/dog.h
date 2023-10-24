3fa69fa62b16031bc10bbe7af4294886ddb672f4
ayaosama290 <orakel999@gmail.com>
2023-10-24 02:01:41 -0700

    h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,8 +17,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 /**
  * dog - typedef for struct dog
  */
-/**
-  typedef struct dog dog;
-  */
+typedef struct dog dog;
 void print_dog(struct dog *d);
 #endif
