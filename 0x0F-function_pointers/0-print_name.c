067eba4ba59a2aac37d03284e56ff7861eff2e5d
ayaosama290 <orakel999@gmail.com>
2023-10-24 06:46:55 -0700

    k

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints name
+ * @name: the first parameter
+ * @f: the second parameter
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
