5781017cee6d6a4fd6f979ba484e85091f730e7c
ayaosama290 <orakel999@gmail.com>
2023-10-24 00:11:52 -0700

    j

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner of the dog
+ */
 struct dog
 {
 	char *name;
