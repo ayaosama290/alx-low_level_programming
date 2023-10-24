818bfc205c6070b79f595ac87d12773a481fb601
ayaosama290 <orakel999@gmail.com>
2023-10-24 09:56:53 -0700

    h

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+/**
+ * main - Entry point
+ * @argc: the first parameter
+ * @argv: the second parameter
+ * Return: an int
+ */
 int main(int argc, char **argv)
 {
 	char *p = (char *)main;
@@ -11,7 +17,7 @@ int main(int argc, char **argv)
 		printf("Error\n"), exit(1);
 	b = atoi(argv[1]);
 	if (b < 0)
-		printf("Error\n"),exit(2);
+		printf("Error\n"), exit(2);
 
 	while (b--)
 		printf("%02hhx%s", *p++, b ? " " : "\n");
