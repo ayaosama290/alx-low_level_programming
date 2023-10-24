93bb8e3c1ba7e27cf467da9794a3347d54228fa4
ayaosama290 <orakel999@gmail.com>
2023-10-24 07:57:50 -0700

    h

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - index of first match element
+ * @array: the first parameter
+ * @size: the second parameter
+ * @cmp: the third parameter
+ * Return: an int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			cmp(array[i]);
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
