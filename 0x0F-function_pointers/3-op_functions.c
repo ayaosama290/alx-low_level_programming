8f305bf4a2e9ae7a832a35efafbd4cb96b73dcc7
ayaosama290 <orakel999@gmail.com>
2023-10-24 09:04:59 -0700

    h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - add numbers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: an int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts numbers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: an int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies numbers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: an int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides numbers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: an int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo numbers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: an int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
