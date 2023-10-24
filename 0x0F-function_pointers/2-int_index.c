8f2df7316f28373c57139051b28b7206a84f4f9b
ayaosama290 <orakel999@gmail.com>
2023-10-24 08:17:41 -0700

    j

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,20 +11,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-	{
-		return (-1);
-	}
-	else
+	if (array && size && cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
-			cmp(array[i]);
-			if (cmp(array[i]) != 0)
-			{
+			if (cmp(array[i])
 				return (i);
-			}
 		}
 	}
-	return (0);
+	return (-1);
 }
