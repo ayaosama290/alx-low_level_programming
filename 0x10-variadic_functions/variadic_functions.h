99339cb1cd657c5ce647b69d9346682b9603ff18
ayaosama290 <orakel999@gmail.com>
2023-10-25 06:23:55 -0700

    h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,20 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct token - a structure
+ * @token: the first parameter
+ * @f: the second parameter
+ */
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+} token_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
