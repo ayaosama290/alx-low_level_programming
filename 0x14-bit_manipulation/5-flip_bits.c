7843c73e9e4e1a3def1aada3e30547adb918dc12
ayaosama290 <orakel999@gmail.com>
2023-10-26 12:16:06 -0700

    p

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - flips bits
+ * @n: first
+ * @m: second
+ * Return: an int
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xorval = n ^ m;
+	unsigned int count = 0;
+
+	while (xorval)
+	{
+		if (xorval & 1ul)
+			count++;
+		xorval = xorval >> 1;
+	}
+	return (count);
+}
