fd7e44463ee5f46e86104b5776d42a6698cff97b
ayaosama290 <orakel999@gmail.com>
2023-10-25 19:34:46 -0700

    o

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -46,7 +46,7 @@ size_t print_listint_safe(const listint_t *head)
 				return (num);
 			}
 		}
-		num++
+		num++;
 		list = _r(list, num, head);
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
