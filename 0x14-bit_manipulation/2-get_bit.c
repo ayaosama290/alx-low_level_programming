caf4ffb0d7534cf8c38621fb4025ba73d840ae1b
ayaosama290 <orakel999@gmail.com>
2023-10-26 11:52:56 -0700

    p

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_bit - gets bit
+ * @n: first
+ * @index: second
+ * Return: an int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) *8)
+		return (-1);
+
+	return (n >> index & 1);
+}
